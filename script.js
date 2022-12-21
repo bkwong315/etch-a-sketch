a2c7a78e00b493e01a5d7961b61c6563cc223272
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-12-21 00:49:19 -0800

    Add logic for board generation and color change

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const createGridItem = (row, col) => {
   const gridItem = document.createElement("div");
   gridItem.style.gridColumn = `${col} / ${col + 1}`;
   gridItem.style.gridRow = `${row} / ${row + 1}`;
+  gridItem.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "orange";
+  });
 
   return gridItem;
 };
@@ -11,4 +14,19 @@ const calcGridSize = (dim) => {
 
   grid.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${dim}, 1fr)`;
+};
+
+const init = () => {
+  const dim = prompt("Enter grid size:");
+  const grid = document.querySelector(".grid");
+
+  calcGridSize(dim);
+
+  let newGridItem;
+  for (let row = 1; row <= dim; row++) {
+    for (let col = 1; col <= dim; col++) {
+      newGridItem = createGridItem(row, col);
+      grid.appendChild(newGridItem);
+    }
+  }
 };
